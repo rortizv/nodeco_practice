2a234055022c4281ca389b12ff64f60ca17b22d6
Rafael <rafael007@gmail.com>
2020-04-06 19:59:00 -0500

    OBTENER RUTA

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http');
+const url = require('url');
 
 const callbackDelServidor = (req, res) => {
+    const urlActual = req.url;
+    const urlParseada = url.parse(urlActual,true);
+    const ruta = urlParseada.pathname;
     // 1. Obtener url desde el objeto request
     // 2. Obtener la ruta
     // 3. Enviar una respuesta dependiendo de la ruta
